e6162ca4a6d9bf960c4c4d5416689e5f032cc011
awesomecosmos <56360465+awesomecosmos@users.noreply.github.com>
2021-07-16 11:22:14 +1200

    update

--- non-sidereal_track_rate.py
+++ non-sidereal_track_rate.py
@@ -2,9 +2,9 @@
 
 from funcs import *
 
-target_id = 'C/2020 O2'
+target_id = 'C/2019 F1'
 obs_code = '474'
-date = Time('2021-07-03 07:45:00') #Time needs to be in UT
+date = Time('2021-07-04 15:00:00') #Time needs to be in UT
 
 JPL_astropy_tbl,my_track_rate_RA,my_track_rate_Dec = JPL_Querier(target_id,obs_code,date)
 converted_track_rate_RA,converted_track_rate_Dec = rate_convertor(my_track_rate_RA,my_track_rate_Dec,'h','s')
