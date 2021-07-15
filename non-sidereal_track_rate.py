df7e1772c5f0112dcf445c40b45b048207162728
awesomecosmos <56360465+awesomecosmos@users.noreply.github.com>
2021-07-16 11:31:49 +1200

    update

--- non-sidereal_track_rate.py
+++ non-sidereal_track_rate.py
@@ -8,5 +8,6 @@ date = Time('2021-07-04 15:00:00') #Time needs to be in UT
 
 JPL_astropy_tbl,my_track_rate_RA,my_track_rate_Dec = JPL_Querier(target_id,obs_code,date)
 converted_track_rate_RA,converted_track_rate_Dec = rate_convertor(my_track_rate_RA,my_track_rate_Dec,'h','s')
+
 print("TraRA: ",converted_track_rate_RA)
 print("TraDec: ",converted_track_rate_Dec)
